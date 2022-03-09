a461815c28ebc0f0635e9b910439e370ff4742ba
Mahershi Bhavsar <mahershi1999@gmail.com>
2022-03-09 00:57:20 -0600

    9 march final

--- crm/crm_app/admin.py
+++ crm/crm_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import UserModel
+from .models import UserModel, MovieModel, TvModel
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
@@ -23,3 +23,41 @@ class UserAdmin(BaseUserAdmin):
     filter_horizontal = ()
     search_fields = ('email', 'name', 'username')
     ordering = ('name',)
+
+
+@admin.register(MovieModel)
+class MovieAdmin(BaseUserAdmin):
+    fieldsets =(
+        (None, {'fields': ('id', 'title')}),
+        (_('Data'), {'fields': ('overview', 'genres', 'tagline', )}),
+    )
+
+    # add_fieldsets = (
+    #     (None, {'fields': ('username', 'password1', 'password2')}),
+    #     (_('Personal Info'), {'fields': ('name', 'email',)}),
+    # )
+
+    list_display = ('id', 'title',)
+    list_filter = ()
+    filter_horizontal = ()
+    search_fields = ('title', )
+    ordering = ('-popularity',)
+
+@admin.register(TvModel)
+class MovieAdmin(BaseUserAdmin):
+    fieldsets =(
+        (None, {'fields': ('id', 'title')}),
+        (_('Data'), {'fields': ('overview', 'genres', 'tagline', )}),
+    )
+
+    # add_fieldsets = (
+    #     (None, {'fields': ('username', 'password1', 'password2')}),
+    #     (_('Personal Info'), {'fields': ('name', 'email',)}),
+    # )
+
+    list_display = ('id', 'title',)
+    list_filter = ()
+    filter_horizontal = ()
+    search_fields = ('title', )
+    ordering = ('title', )
+
